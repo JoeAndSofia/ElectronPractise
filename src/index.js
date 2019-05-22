a0ce8a914a7af0fd158e88434ba33a9ffa94da83
BearJoeWYL <wylsniper@163.com>
2019-05-22 17:31:13 +0800

    master 2019-05-22 17:30:51 window's menu displaying control added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -6,13 +6,16 @@ let win
 
 function createWindow () {
 	// 创建浏览器窗口。
-	win = new BrowserWindow({width: 800, height: 600})
+	win = new BrowserWindow({width: 800, height: 600, frame:false});
 	
 	// 然后加载应用的 index.html。
-	win.loadFile('src/index.html')
+	win.loadFile('src/index.html');
 	
 	// 打开开发者工具
 	// win.webContents.openDevTools();
+
+	//隐藏Menu
+	Menu.setApplicationMenu(null);
 	
 	// 当 window 被关闭，这个事件会被触发。
 	win.on('closed', () => {
