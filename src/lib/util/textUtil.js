b9cab2ab86971ef5e4de60c251e96d62ae4687ee
BearJoeWYL <wylsniper@163.com>
2019-07-17 18:04:19 +0800

    master 2019-07-17 18:03:55 stage commit

--- src/lib/util/textUtil.js
+++ src/lib/util/textUtil.js
@@ -20,6 +20,111 @@ const formatIntegerFillWithZero = (integerValue = 0, length) => {
 	}
 	return result
 
-}
+};
+
+const DETAIL_INFO_TEMPLATE = {
+	promo_filter:{
+		sponsor: ["PARTNERSHIP_EVENT_SPONSOR", "NO_SPONSOR"]
+	},
+	review_filter:{
+		rating: {
+			RATING_MAX:0,
+			RATING_MIN:0
+		}
+	},
+	official_account_list: {
+		title: "",
+		description: "",
+		banner: "",
+		show_followable_first: 0,
+		available_sorting: [
+			0,
+			1,
+			2,
+			3,
+			4
+		],
+		default_sorting: 0,
+	},
+	check_number:1,
+	check_string:"1",
+	check_boolean:true,
+	check_null:null,
+	check_undefined:undefined,
+	check_NaN:NaN
+};
+
+const getUnexpectedDiff = (obj, templateObj) => {
+	let diff = null;
+	if(!obj){
+		return diff;
+	}else{
+		for(let i in obj){
+			let tempDiff = null;
+			if(obj.hasOwnProperty(i)){
+				if(!(templateObj.hasOwnProperty(i))){
+					tempDiff = obj[i];
+				}else{
+					// console.log(obj[i], templateObj[i], typeof obj[i], typeof templateObj[i]);
+					if(typeof obj[i] === typeof templateObj[i]){
+						if(obj[i] === null && templateObj[i] !== null){
+							if(diff === null){
+								diff = {};
+							}
+							diff[i] = null;
+						}else{
+							let objIsArray = Array.isArray(obj[i]);
+							let targetIdArr = Array.isArray(templateObj[i]);
+							if(objIsArray === targetIdArr){
+								if(objIsArray){
+
+								}else{
+									let tempDiff = getUnexpectedDiff(obj[i], templateObj[i])
+								}
+
+							}else{
+								tempDiff = obj[i];
+							}
+						}
+					}else{
+						tempDiff = obj[i];
+					}
+				}
+			}
+			if(tempDiff !== null){
+				if(diff === null){
+					diff = {};
+				}
+				diff[i] = tempDiff;
+			}
+		}
+		return diff;
+	}
+};
+
+let testObj = {
+	promo_filter:{
+		sponsor: ["PARTNERSHIP_EVENT_SPONSOR", "NO_SPONSOR"]
+	},
+	review_filter:null,
+	unexpected_array: [],
+	check_number:2,
+	check_string:"",
+	check_boolean:false,
+	check_null:null,
+	check_undefined:undefined,
+	check_NaN:NaN
+};
+
+console.log(null, typeof null);
+console.log(undefined, typeof undefined);
+console.log(NaN, typeof NaN);
+console.log("1", typeof "1");
+console.log(true, typeof true);
+console.log(1, typeof 1);
+console.log(()=> {}, typeof (()=> {}));
+console.log({}, typeof {});
+
+console.log('result: ', getUnexpectedDiff(testObj, DETAIL_INFO_TEMPLATE));
 
 exports.formatIntegerFillWithZero = formatIntegerFillWithZero;
